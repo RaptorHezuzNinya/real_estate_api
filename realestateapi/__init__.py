273e1f52b094990a699d12b3906959f300f0b94a
raptorzhezuzninya <w22243vrh@gmail.com>
2018-08-12 22:35:24 +0200

    fixing imports

--- realestateapi/__init__.py
+++ realestateapi/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,3 +6,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/real_estate_api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# 13:32 min
--- realestateapi/routes.py
+++ realestateapi/routes.py
@@ -1,3 +1,5 @@
+from flask import Flask, render_template, request
+
 @app.route('/')
 def index():
     return render_template('index.html')
