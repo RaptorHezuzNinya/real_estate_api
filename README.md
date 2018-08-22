d246a6d1f8176dc4d36e993f1756857fbf9b4594
raptorzhezuzninya <w22243vrh@gmail.com>
2018-08-23 00:56:05 +0200

    update readme lol

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 API to automate tasks
+
+have python 3.7
+install pip
+source virtual env 
+pip install requirements.txt
+
+(give or take hehe)
--- realestateapi/models.py
+++ realestateapi/models.py
@@ -28,3 +28,17 @@ class Upload(db.Model):
 
     def __repr__(self):
         return f"Upload('{self.id}', '{self.json_file}')"
+
+
+class Tenant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    account_holder = db.Column(db.String(150), unique=True)
+    first_name = db.Column(db.String(25))
+    last_name = db.Column(db.String(35))
+    iban = db.Column(db.String(35), nullable=False)
+    rent = db.Column(db.Float)
+    phone = db.Column(db.String(12))
+
+    def __repr__(self):
+        return f"Tenant('{self.id}','{self.first_name}','{self.last_name}','{self.iban}','{self.rent}','{self.phone}')"
