1b40b40f4bb1352f47083c9887ba4a3f7049f663
raptorzhezuzninya <w22243vrh@gmail.com>
2018-08-22 22:51:40 +0200

    refacotor upload route with helpers

--- realestateapi/routes.py
+++ realestateapi/routes.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, jsonify
 from realestateapi.models import Tenant, Payment, Upload
 from realestateapi import app, db
 from modules.parser import Parser
+from modules.csv2json import Csv2json
 import json
-import csv
 import datetime
+import os
 
 
 @app.route('/')
@@ -16,37 +17,41 @@ def index():
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
+
         if is_csv(file.filename):
-            file.save(
-                '/Users/RaptorHezuzNinya/code/python/real_estate_API/uploads/' + file.filename)
-            json_list = convert(file.filename)
-            return jsonify(json_list)
+            cwd = os.getcwd()
+            path = cwd + '/uploads/' + file.filename
+
+            save_file(file, path)
+
+            return handle_csv(path)
+
+        return handle_json(file)
 
 
-def convert(filename):
-    with open('/Users/RaptorHezuzNinya/code/python/real_estate_API/uploads/' + filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        dict_list = []
-        for line in reader:
-            dict_list.append(line)
-        return dict_list
+def save_file(file, path):
+    return file.save(path)  # file is saved to uploads dir
 
 
 def is_csv(file_name):
     return True if file_name[-3:] == 'csv' else False
 
 
-# @app.route('/uploadfile', methods=['POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         json_list = json.load(file)
-#         datetime_now = datetime.datetime.now()
-#         new_upload = Upload(json_file=json_list, uploaded_at=datetime_now)
-#         db.session.add(new_upload)
-#         db.session.commit()  # new json upload is now in db;
-#         all_uploads_desc = Upload.query.order_by(Upload.id.desc())
-#         last_upload = all_uploads_desc.first()
-#         p = Parser(last_upload.json_file)
-#         p.loop_payments()
-#         return jsonify(last_upload.json_file)
+def handle_csv(path):
+    c2j = Csv2json(path)
+    result = c2j.get_json()
+    return jsonify(output=result)
+
+
+def handle_json(file):
+    json_list = json.load(file)
+    datetime_now = datetime.datetime.now()
+    new_upload = Upload(json_file=json_list, uploaded_at=datetime_now)
+    db.session.add(new_upload)
+    db.session.commit()  # new json upload is now in db;
+    all_uploads_desc = Upload.query.order_by(Upload.id.desc())
+    last_upload = all_uploads_desc.first()
+    p = Parser(last_upload.json_file)
+    p.loop_payments()
+    j = last_upload.json_file
+    return jsonify(output=j)
