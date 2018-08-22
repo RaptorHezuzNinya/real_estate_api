693e86ffa19bdb6f408d0101c0dbf1a9ef703307
raptorzhezuzninya <w22243vrh@gmail.com>
2018-08-22 19:02:54 +0200

    made it work

--- realestateapi/routes.py
+++ realestateapi/routes.py
@@ -17,40 +17,32 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if is_csv(file.filename):
-            json_list = convert(file)
-            return jsonify(output=json_list)
+            file.save(
+                '/Users/RaptorHezuzNinya/code/python/real_estate_API/uploads/' + file.filename)
+            json_list = convert(file.filename)
+            return jsonify(json_list)
 
 
-def convert(file):
-    # csvfile = open(
-    #     '/Users/RaptorHezuzNinya/code/python/real_estate_API/data/smalldata.csv', 'r')
-    # jsonfile = []
-
-    # fieldnames = ("Datum", "Naam / Omschrijving", "Rekening", "Tegenrekening",
-    #               "Code", "Af Bij", "Bedrag (EUR)", "MutatieSoort", "Mededelingen")
-    # reader = csv.DictReader(csvfile, fieldnames)
-    # for row in reader:
-    #     json.dump(row, jsonfile + ',')
-    #     jsonfile.write('\n')
-    reader = csv.DictReader(file)
-    dict_list = []
-    for line in reader:
-        dict_list.append(line)
-    return dict_list
+def convert(filename):
+    with open('/Users/RaptorHezuzNinya/code/python/real_estate_API/uploads/' + filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        dict_list = []
+        for line in reader:
+            dict_list.append(line)
+        return dict_list
 
 
 def is_csv(file_name):
-    extension = file_name[-3:]
-    return True if extension == 'csv' else False
+    return True if file_name[-3:] == 'csv' else False
 
 
 # @app.route('/uploadfile', methods=['POST'])
 # def upload_file():
 #     if request.method == 'POST':
 #         file = request.files['file']
-#         json_data = json.load(file)
+#         json_list = json.load(file)
 #         datetime_now = datetime.datetime.now()
-#         new_upload = Upload(json_file=json_data, uploaded_at=datetime_now)
+#         new_upload = Upload(json_file=json_list, uploaded_at=datetime_now)
 #         db.session.add(new_upload)
 #         db.session.commit()  # new json upload is now in db;
 #         all_uploads_desc = Upload.query.order_by(Upload.id.desc())
