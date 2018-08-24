7112f1c5c6ab386c7706fd603114303c8c3244dc
raptorzhezuzninya <w22243vrh@gmail.com>
2018-08-25 01:48:37 +0200

    add route

--- realestateapi/routes.py
+++ realestateapi/routes.py
@@ -8,6 +8,13 @@ import datetime
 import os
 
 
+@app.route('/tenants')
+def get_tenants():
+    tenant = Tenant()
+    tenants = tenant.query.all()
+    return jsonify(output=tenants[0])
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -40,23 +47,32 @@ def is_csv(file_name):
 def handle_csv(path):
     c2j = Csv2json(path)
     json_list = c2j.get_json()
-    return insert_db(json_list)
+    insert_db(json_list)
+    return run_parser()
 
 
 def handle_json(file):
     json_list = json.load(file)
-    return insert_db(json_list)
+    insert_db(json_list)
+    return run_parser()
 
-    # all_uploads_desc = Upload.query.order_by(Upload.id.desc())
-    # last_upload = all_uploads_desc.first()
-    # p = Parser(last_upload.json_file)
-    # p.loop_payments()
+
+def run_parser():
+    last_upload = get_last_upload()
+    p = Parser(last_upload.upload_content)
+    p.loop_payments()
+    return 'parsing complete'
 
 
 def insert_db(payments_json):
     datetime_now = datetime.datetime.now()
-    # change column name to something else its list with dict json objects  UPLOAD_CONTENT
-    new_upload = Upload(json_file=payments_json, uploaded_at=datetime_now)
+    new_upload = Upload(upload_content=payments_json, uploaded_at=datetime_now)
     db.session.add(new_upload)
-    db.session.commit()  # new js
-    return 'sucess uniconr'
+    db.session.commit()
+    return 'upload successful'
+
+
+def get_last_upload():
+    all_uploads_desc = Upload.query.order_by(Upload.id.desc())
+    last_upload = all_uploads_desc.first()
+    return last_upload
