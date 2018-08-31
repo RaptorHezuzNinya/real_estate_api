9dc51fcf7d9359747f8d48b86825c4da8707c1b0
RaptorHezuzNinya <w22243vrh@gmail.com>
2018-08-31 12:51:42 +0200

    add db config line for linux

--- realestateapi/__init__.py
+++ realestateapi/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/real_estate_api'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/real_estate_api' #macOs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ward:password@localhost/ward'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
