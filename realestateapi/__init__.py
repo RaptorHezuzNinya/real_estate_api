e4e1607fb2a8d25cbda67b00d82f574fac9b22d6
RaptorHezuzNinya <w22243vrh@gmail.com>
2018-11-01 10:48:47 +0100

    removed redundant db config

--- realestateapi/__init__.py
+++ realestateapi/__init__.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 if platform.system() == 'Darwin':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/real_estate_api' #macOs
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ward:password@localhost/ward'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/real_estate_api' #`macOs
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ward:password@localhost/ward' #linux
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
